fef4251df0c14a793fce36deed7f515a51f8e548
grandpariley <rileyherman@duck.com>
2024-09-26 16:10:05 -0600

    grrrrr

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def arch2():
     solutions = po.main.main({
         'arch2': default_portfolio_optimization_problem_arch_2(),
     })
-    await db.insert_arch2_portfolios(solutions['name'])
+    await db.insert_arch2_portfolios(solutions['arch2'])
 
 
 app.add_event_handler(event_type='startup', func=arch2)
