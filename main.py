17425a44e41e779b90b006efce050c7490cf6c2b
grandpariley <rileyherman@duck.com>
2024-09-26 11:23:54 -0600

    I think this prevents the timeouts

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def survey(survey_result: Response):
 
 
 @app.get("/api/v1/portfolio/{portfolio_id}/status")
-def status(portfolio_id: str):
+async def status(portfolio_id: str):
     if asyncio.run(db.portfolio_exists(portfolio_id)):
         return {'status': 'READY'}
     return {'status': 'PENDING'}
