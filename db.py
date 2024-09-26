b1645e6c5a2e24a739d6f2177d1d77b7260d779b
grandpariley <rileyherman@duck.com>
2024-09-26 16:01:00 -0600

    oops

--- db.py
+++ db.py
@@ -59,7 +59,7 @@ async def find_all(cursor):
 
 
 async def insert_arch2_portfolios(solutions):
-    await arch2_portfolio.insert_one(solutions)
+    await arch2_portfolio.insert_one(list(map(problem_encoder_fn, solutions)))
 
 
 async def get_arch2_portfolios():
