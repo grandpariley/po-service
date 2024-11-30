49d64c8a7dfbbef21ab3f41c54abf7abea1ef745
grandpariley <rileyherman@duck.com>
2024-11-30 17:15:40 -0600

    changing to run directly

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.12
+FROM python:3.10
 WORKDIR /code
 COPY ./Pipfile* /code/
 RUN pip install pipenv && pipenv install --deploy --ignore-pipfile
--- main.sh
+++ main.sh
@@ -1,2 +1,4 @@
-sudo docker build -t po-service-container .
-sudo docker run --env-file .env -p 81:80 po-service-container
+#sudo docker build -t po-service-container .
+#sudo docker run --env-file .env -p 81:80 po-service-container
+
+pipenv run fastapi run main.py --port 80
