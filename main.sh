48b72c6259b7bdf35d2042f54c02763f382b8838
grandpariley <rileyherman@duck.com>
2024-11-30 18:25:21 -0600

    and watch it toooo

--- main.sh
+++ main.sh
@@ -2,3 +2,4 @@
 #sudo docker run --env-file .env -p 81:80 po-service-container
 pip install pipenv && pipenv install --deploy --ignore-pipfile
 pipenv run fastapi run main.py --port 2376 &> output.log &
+tail -f output.log
