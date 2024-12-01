31702361464ec9bdde99ea718a9583d972feac35
grandpariley <rileyherman@duck.com>
2024-11-30 18:23:30 -0600

    whoopsie lost the logs

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #sudo docker build -t po-service-container .
 #sudo docker run --env-file .env -p 81:80 po-service-container
 pip install pipenv && pipenv install --deploy --ignore-pipfile
-pipenv run fastapi run main.py --port 80
+pipenv run fastapi run main.py --port 2376 &> output.log &
