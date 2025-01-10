b00339cb1125cf453e3f76387d5e46cd2511dc70
grandpariley <rileyherman@duck.com>
2025-01-09 19:26:17 -0600

    adsf

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #sleep 60
-pipenv run gunicorn -w 1 -b 0.0.0.0:2736 --access-logfile - --error-logfile - main:app
+#pipenv run \
+gunicorn -w 1 -b 0.0.0.0:2736 --access-logfile - --error-logfile - helper:app
