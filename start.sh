daddb580ff0ddeed63025ebdff7a878b103e2b81
grandpariley <rileyherman@duck.com>
2025-01-09 18:44:36 -0600

    afds

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-sleep 60 && pipenv run gunicorn -w 1 -b 0.0.0.0:2736 --access-logfile - --error-logfile - main:app
+#sleep 60
+pipenv run gunicorn -w 1 -b 0.0.0.0:2736 --access-logfile - --error-logfile - main:app
