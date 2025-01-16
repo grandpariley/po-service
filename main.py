e1158f3e554afe6cc2cd30a01014a784280efd01
grandpariley <rileyherman@duck.com>
2025-01-15 21:41:33 -0600

    remove unecessary logs

--- main.py
+++ main.py
@@ -52,8 +52,6 @@ async def portfolio_optimization(portfolio_id):
 async def get_portfolio_weights(portfolio_id):
     all_responses = await db.get_surveys()
     all_weights = get_weights(get_responses(all_responses))
-    Log.log("1> " + str(all_weights))
-    Log.log("2> " + str(get_weights_by_portfolio_id(all_weights, portfolio_id)))
     weights = get_weights_by_portfolio_id(all_weights, portfolio_id)
     return weights
 
