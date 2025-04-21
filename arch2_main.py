d961980ebe8139bbe21b023e0e52071788ef2fb8
grandpariley <rileyherman@tuta.io>
2025-04-21 12:16:55 -0600

    dammit python auto import

--- arch2_main.py
+++ arch2_main.py
@@ -1,13 +1,13 @@
 import asyncio
 
 import db
-import po
+from po.main import main
 from po.pkg.problem.builder import default_portfolio_optimization_problem_arch_2
 
 
 async def arch2():
     await db.clear_arch2_portfolio()
-    solutions = await po.main.main({
+    solutions = await main({
         'arch2': default_portfolio_optimization_problem_arch_2(),
     })
     await db.insert_arch2_portfolios(solutions['arch2'])
